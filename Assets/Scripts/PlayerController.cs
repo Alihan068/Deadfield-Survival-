e0404d0c3518c2118455de8cee3e041d9597d655
Alihan <alihankomurcuoglu@gmail.com>
2025-10-27 17:52:35 +0300

    DashTest

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,21 +3,28 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+    PlayerStatsManager playerStatsManager;
     Rigidbody2D rb2d;
     Animator animator;
 
-    [SerializeField] float moveSpeed = 5.0f; 
+    [SerializeField] float moveSpeed = 5.0f;
+    [SerializeField] float dashPower = 10f;
 
     Vector3 moveInput;
     bool isAlive = true;
     void Start()
     {
-
+        playerStatsManager = GetComponent<PlayerStatsManager>();
         rb2d = GetComponent<Rigidbody2D>();
     }
     void OnMove(InputValue value) {
         if (!isAlive) return;
         moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+    void OnDash(InputValue value) {
+        Dash();
     }
 
     // Update is called once per frame
@@ -31,14 +38,15 @@ public class PlayerController : MonoBehaviour
 
         Vector2 vector = rb2d.linearVelocity;
         vector.x = moveInput.x * moveSpeed;
-        vector.y = moveInput.z * moveSpeed;
+        vector.y = moveInput.y * moveSpeed;
         rb2d.linearVelocity = vector;
 
         if (animator) {
             bool hasHorizontal = Mathf.Abs(vector.x) > Mathf.Epsilon;
             animator.SetBool("isWalking", hasHorizontal);
         }
-
-
+    }
+    void Dash() {
+        rb2d.AddForce(moveInput * dashPower, ForceMode2D.Impulse);
     }
 }
